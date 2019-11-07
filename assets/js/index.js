1ac4f143e13f239db30adfe193f6103d3c1c145c
jayken.xie <jaykenxie@outlook.com>
2019-11-07 20:59:43 +0800

    完善下拉框显示

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,17 +2,12 @@ hljs.initHighlightingOnLoad();
 (function() {
   const userInfoDrop = document.querySelector(".user-info-drop");
   const userInfoTitle = document.querySelector(".user-info_username");
-  let timer = null;
   userInfoTitle && userInfoTitle.addEventListener("mouseover", function() {
     userInfoDrop.style.display = "block";
-    timer = setTimeout(() => {
-      userInfoDrop.style.display = "none";
-    }, 3000);
   });
-  // userInfoDrop.addEventListener("mouseover", function() {
-  //   clearTimeout(timer);
-  // });
-  // userInfoDrop.addEventListener("mouseout", function(evet) {
-  //   userInfoDrop.style.display = "none";
-  // });
+  userInfoTitle && document.body.addEventListener('click', function(evet) {
+    if(!userInfoDrop.contains(evet.target)) {
+      userInfoDrop.style.display = "none";
+    }
+  })
 })();
