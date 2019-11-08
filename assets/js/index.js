71d5a6bd36430ae8892d45313b1fa7906a7df3c8
jayken.xie <jaykenxie@outlook.com>
2019-11-08 16:22:05 +0800

    const替换成var

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 hljs.initHighlightingOnLoad();
 (function() {
-  const userInfoDrop = document.querySelector(".user-info-drop");
-  const userInfoTitle = document.querySelector(".user-info_username");
+  var userInfoDrop = document.querySelector(".user-info-drop");
+  var userInfoTitle = document.querySelector(".user-info_username");
   userInfoTitle &&
     userInfoTitle.addEventListener("mouseover", function() {
       userInfoDrop.style.display = "block";
@@ -13,9 +13,9 @@ hljs.initHighlightingOnLoad();
       }
     });
   // 实现图片预览
-  const modal = document.querySelector(".modal");
-  const modalImg = document.querySelector(".modal-box img");
-  const postContent = document.querySelector(".post-content");
+  var modal = document.querySelector(".modal");
+  var modalImg = document.querySelector(".modal-box img");
+  var postContent = document.querySelector(".post-content");
   postContent &&
     postContent.addEventListener("click", function(evet) {
       if (evet.target.tagName === "IMG") {
